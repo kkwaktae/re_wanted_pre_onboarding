d4725b80378020b6d719d09a42538aeb3221ab4d
taehoon <ender4@naver.com>
2022-05-08 15:51:40 +0900

    working: slider feature

--- src/pages/slider/Slider.js
+++ src/pages/slider/Slider.js
@@ -1,14 +1,37 @@
+// import { useState } from 'react';
 import styles from './Slider.module.scss';
+import { PERCENT_DATA } from './data/data';
 
 function Slider() {
+  // const [distance, setDistance] = useState(0);
+  const BAR_DISTANCE = 300;
+  console.log(BAR_DISTANCE * 0, BAR_DISTANCE * 0.25, BAR_DISTANCE * 0.5, BAR_DISTANCE * 0.75, BAR_DISTANCE * 1);
+
+  const points = [...PERCENT_DATA].map((percent) => {
+    return <li key={`point-number-${percent.id}`} className={styles.point} />;
+  });
+  const buttons = [...PERCENT_DATA].map((percent) => {
+    return (
+      <button key={`percentage-btn-number-${percent.id}`} type='button' className={styles.btn}>
+        {percent.percentage}%
+      </button>
+    );
+  });
+
   return (
     <div className={styles.sliderContainer}>
       <div className={styles.statsViewer}>
-        <p className={styles.figure}>50</p>
+        <p className={styles.figure}>1</p>
         <span className={styles.percent}>%</span>
       </div>
-      <ul className={styles.slider} />
-      <ul className={styles.percentBtn} />
+
+      <ul className={styles.slider}>
+        {points}
+        <li className={styles.currentBar} />
+        <li className={styles.basedBar} />
+      </ul>
+
+      <div className={styles.percentBtn}>{buttons}</div>
     </div>
   );
 }
---
+++ src/pages/slider/data/data.js
@@ -0,0 +1,22 @@
+export const PERCENT_DATA = [
+  {
+    id: 1,
+    percentage: 1,
+  },
+  {
+    id: 2,
+    percentage: 25,
+  },
+  {
+    id: 3,
+    percentage: 50,
+  },
+  {
+    id: 4,
+    percentage: 75,
+  },
+  {
+    id: 5,
+    percentage: 100,
+  },
+];
